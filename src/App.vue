cef1e39563b3bdeea16e1d27e8d715ea8c73f651
francescorossi1 <rossi.francesco.dev@gmail.com>
2022-08-23 12:33:24 +0200

    created BaseHeader component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
   <div>
-    
+    <BaseHeader />
   </div>
 </template>
 
 <script>
+import BaseHeader from './components/BaseHeader.vue';
 
 
 export default {
   name: 'App',
   components: {
-  }
+    BaseHeader
+}
 }
 </script>
 
